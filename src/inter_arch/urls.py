d961566979867ca43f1588b00afeee1ce49a1ce3
Jairo Alan Castelan Fernandez <jairoalanc@outlook.com>
2024-03-11 11:23:47 -0600

    ya se ve Much

--- src/inter_arch/urls.py
+++ src/inter_arch/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from arch_int import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home_view,name='InterfazArachnids')
 ]
 
 
